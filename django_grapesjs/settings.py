a484e74d24920982aeaa2af850193aae51f1c204
TheLazzziest <mxyakovenko9@gmail.com>
2019-09-30 20:04:46 +0300

    Add django_npm library

--- django_grapesjs/settings.py
+++ django_grapesjs/settings.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 
+INSTALLED_APPS = getattr(settings, 'INSTALLED_APPS', []) + ['django_npm',]
 
 # path to the html file of the form field. Enter your value for the override
 GRAPESJS_TEMPLATE = getattr(settings, 'GRAPESJS_TEMPLATE', 'django_grapesjs/textarea.html')
@@ -23,8 +24,26 @@ REDACTOR_CONFIG = {
     **getattr(settings, 'REDACTOR_CONFIG', {})
 }
 
+STATICFILE_FINDERS = getattr(settings, 'STATICFILE_FINDERS', []) + ['npm.finders.NpmFinder',]
+
 TEMPLATE_DIR = getattr(settings, 'TEMPLATE_DIR', 'templates')
 STATIC_URL = getattr(settings, 'STATIC_URL', "/static/")
+
+# Configurations of django_npm
+
+# absolute path to the npm "root" directory
+NPM_ROOT_PATH = getattr(settings, 'NPM_ROOT_PATH', None)
+
+#  an absolute path to npm executable
+NPM_EXECUTABLE_PATH = getattr(settings, 'NPM_ROOT_PATH', None)
+
+# A url prefix for static files in node_modules, e.g.:
+# os.path.join('js', 'lib') so your files will be in /static/js/lib/react.js for example
+NPM_STATIC_FILES_PREFIX = getattr(settings, 'NPM_STATIC_FILES_PREFIX', '')
+
+# Specify which files should be picked up in static
+# By default, all are exposed
+NPM_FILE_PATTERNS = getattr(settings, 'NPM_FILE_PATTERNS', {})
 
 # you can override the name of tags
 NAME_IGNORE_TAG = getattr(settings, 'NAME_IGNORE_TAG', 'ignore')
