2b0df54f01fafa9a134bfcbeff61cf46999412e9
TheLazzziest <mxyakovenko9@gmail.com>
2019-10-07 12:48:04 +0300

    Add helper function for building url for GrapesJS library and provigin GrapesJS assets.

--- django_grapesjs/utils/get_source.py
+++ django_grapesjs/utils/get_source.py
@@ -1,8 +1,14 @@
 from django.template.loader import render_to_string
+
+from django_grapesjs import settings
 from django_grapesjs.template import template_source
 from django_grapesjs.utils import apply_string_handling
 
-__all__ = ('get_render_html_value', )
+__all__ = (
+    'get_render_html_value',
+    'get_grapejs_assets',
+    'build_url'
+)
 
 
 def get_render_html_value(default_html, apply_django_tag=False):
@@ -16,3 +22,26 @@ def get_render_html_value(default_html, apply_django_tag=False):
 
     return _get_render_html_value
 
+
+def get_grapejs_assets(type='all'):
+    """Provide grapes.js asset of a different type."""
+    assets = {
+        **{'core': settings.GRAPESJS_CORE_ASSETS},
+        **settings.GRAPESJS_PLUGIN_ASSETS
+    }
+
+    if type == 'css' or type == 'js':
+        return map(lambda plugin_assets: plugin_assets[type], assets.values())
+
+    return assets
+
+
+def build_url(base: str, arg: str=None):
+    """Build client-side urls for grapes.js library."""
+    if not arg:
+        return base
+    parts = list(
+        filter(lambda part: part, base.split('/'))
+    )
+    parts.append(arg)
+    return "/" + "/".join(parts)
